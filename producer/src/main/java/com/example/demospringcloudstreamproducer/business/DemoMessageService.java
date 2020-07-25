6508af5b566fb1970873ecdcdaf9d1f8bb5953a3
Adonis Cesar Legón Campo <alegon@gmail.com>
2020-07-25 15:14:38 -0400

    minor refactor

--- producer/src/main/java/com/example/demospringcloudstreamproducer/business/DemoMessageService.java
+++ producer/src/main/java/com/example/demospringcloudstreamproducer/business/DemoMessageService.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class StreamMessageService implements MessageService {
+public class DemoMessageService implements MessageService {
 
     @Autowired
     MessageProducer messageProducer;
